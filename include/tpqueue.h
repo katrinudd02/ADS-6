7a1a6219b94c832b9f39a671e605a75ead233337
katrinudd02 <97793336+katrinudd02@users.noreply.github.com>
2022-05-02 19:27:03 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,30 +4,30 @@
 
 template<typename T, int size>
 class TPQueue {
-    private:
-        T arr[1000];
-        int begin, end;
+ private:
+    T arr[1000];
+    int begin, end;
 
-    public:
-        TPQueue() :begin(0), end(0) {}
-        int isSize() {
-            return end - begin;
-        }
-        void push(T value) {
-            int temp = end++;
-            bool flag = true;
-            while (flag) {
-                if ((begin <= temp) && (value.prior > arr[temp % size].prior)) {
-                    arr[(temp + 1) % size] = arr[temp % size];
-                } else {
-                    count = false;
-                }
+ public:
+    TPQueue() :begin(0), end(0) {}
+    int isSize() {
+        return end - begin;
+    }
+    void push(T value) {
+        int temp = end++;
+        bool flag = true;
+        while (flag) {
+            if ((begin <= temp) && (value.prior > arr[temp % size].prior)) {
+                arr[(temp + 1) % size] = arr[temp % size];
+            } else {
+                flag = false;
             }
-            arr[(temp + 1) % size] = value;
         }
-        T pop() {
-            return arr[(begin++) % size];
-        }
+        arr[(temp + 1) % size] = value;
+    }
+    T pop() {
+        return arr[(begin++) % size];
+    }
 };
 
 struct SYM {
